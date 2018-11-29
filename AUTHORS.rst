ebade3f4da7faac0ade7c6807702a2d19016374b
kdavis-mozilla <kdavis@mozilla.com>
2018-11-29 05:59:13 +0100

    Initial commit

---
+++ AUTHORS.rst
@@ -0,0 +1,5 @@
+============
+Contributors
+============
+
+* kdavis-mozilla <kdavis@mozilla.com>
---
+++ tests/test_skeleton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from corporacreator.skeleton import fib
+
+__author__ = "kdavis-mozilla"
+__copyright__ = "kdavis-mozilla"
+__license__ = "mit"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
