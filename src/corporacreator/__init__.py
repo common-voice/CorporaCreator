a2edf812eef7bad9549f2b405c13cd882bc1101d
kdavis-mozilla <kdavis@mozilla.com>
2018-11-30 19:12:38 +0100

    Removed __version__

--- src/corporacreator/__init__.py
+++ src/corporacreator/__init__.py
@@ -1,13 +1,4 @@
 # -*- coding: utf-8 -*-
 from .corpora import Corpora
+from .argparse import parse_args
 from .logging import setup_logging
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = 'CorporaCreator'
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = 'unknown'
-finally:
-    del get_distribution, DistributionNotFound
