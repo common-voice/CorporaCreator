59973852f2b19c6fcd5c23808b1a94d2f57e1090
kdavis-mozilla <kdavis@mozilla.com>
2018-11-30 19:13:54 +0100

    Moved parse_args() to it's own module

---
+++ src/corporacreator/argparse.py
@@ -0,0 +1,57 @@
+import logging
+import argparse
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+__license__ = "mpl"
+__author__ = "kdavis-mozilla"
+__copyright__ = "kdavis-mozilla"
+_logger = logging.getLogger(__name__)
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = 'CorporaCreator'
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="Creates tsv files for Common Voice corpora")
+    parser.add_argument(
+        '--version',
+        action='version',
+        version='CorporaCreator {ver}'.format(ver=__version__))
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_const',
+        required=False,
+        help="set loglevel to INFO",
+        dest="loglevel",
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        action='store_const',
+        required=False,
+        help="set loglevel to DEBUG",
+        dest="loglevel",
+        const=logging.DEBUG)
+    parser.add_argument(
+        '-f',
+        '--file',
+        required=True,
+        help="Path to the Common Voice tsv for all languages",
+        dest="tsvfile")
+    return parser.parse_args(args)
--- src/corporacreator/tool.py
+++ src/corporacreator/tool.py
@@ -1,54 +1,14 @@
 import sys
 import logging
-import argparse
 
 from corporacreator import Corpora
-from corporacreator import __version__
+from corporacreator import parse_args
 from corporacreator import setup_logging
 
 __license__ = "mpl"
 __author__ = "kdavis-mozilla"
 __copyright__ = "kdavis-mozilla"
 _logger = logging.getLogger(__name__)
-
-
-def parse_args(args):
-    """Parse command line parameters
-
-    Args:
-      args ([str]): command line parameters as list of strings
-
-    Returns:
-      :obj:`argparse.Namespace`: command line parameters namespace
-    """
-    parser = argparse.ArgumentParser(description="Creates tsv files for Common Voice corpora")
-    parser.add_argument(
-        '--version',
-        action='version',
-        version='CorporaCreator {ver}'.format(ver=__version__))
-    parser.add_argument(
-        '-v',
-        '--verbose',
-        action='store_const',
-        required=False,
-        help="set loglevel to INFO",
-        dest="loglevel",
-        const=logging.INFO)
-    parser.add_argument(
-        '-vv',
-        '--very-verbose',
-        action='store_const',
-        required=False,
-        help="set loglevel to DEBUG",
-        dest="loglevel",
-        const=logging.DEBUG)
-    parser.add_argument(
-        '-f',
-        '--file',
-        required=True,
-        help="Path to the Common Voice tsv for all languages",
-        dest="tsvfile")
-    return parser.parse_args(args)
 
 
 def main(args):
