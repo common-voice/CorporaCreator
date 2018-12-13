a7294596a09f0d0db6fec2c31eeaa996ead72356
kdavis-mozilla <kdavis@mozilla.com>
2018-12-13 06:56:32 +0100

    Fixed #14 (Some sentences contain URL encoded text)

--- src/corporacreator/preprocessors/common.py
+++ src/corporacreator/preprocessors/common.py
@@ -1,3 +1,5 @@
+from urllib.parse import unquote
+
 def common(sentence):
     """Cleans up the passed sentence in a language independent manner, removing or reformatting invalid data.
 
@@ -7,5 +9,7 @@ def common(sentence):
     Returns:
       (str): Cleaned up sentence.
     """
+    # Decode any URL encoded elements of sentence
+    sentence = unquote(sentence)
     # TODO: Clean up data in a language independent manner
     return sentence
