4cc4f96cdebe4049663b492e60f70ef3bdfb81ed
josh <jmeyer@mozilla.com>
2019-01-29 21:22:05 +0300

    tatar

--- src/corporacreator/preprocessors/tt.py
+++ src/corporacreator/preprocessors/tt.py
@@ -8,5 +8,17 @@ def tt(client_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
-    # TODO: Clean up tt data
+    # this if loop will skip the following else loop in the case that
+    # the sentence is purely Tatar characters as per https://github.com/
+    # unicode-cldr/cldr-misc-full/blob/master/main/tt/characters.json
+    if set(list(sentence)).issubset(set([' ', 'а', 'ә', 'б', 'в', 'г', 'д', 'е',
+                                         'ё', 'ж', 'җ', 'з', 'и', 'й', 'к', 'л',
+                                         'м', 'н', 'ң', 'о', 'ө', 'п', 'р', 'с',
+                                         'т', 'у', 'ү', 'ф', 'х', 'һ', 'ц', 'ч',
+                                         'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю',
+                                         'я'])):
+        pass
+    else:
+        # TODO: Clean up tatar data
+        pass
     return sentence
