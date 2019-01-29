53f5955f5adcb32b7b284b463da1292dc3122558
josh <jmeyer@mozilla.com>
2019-01-29 20:20:18 +0300

    french

--- src/corporacreator/preprocessors/fr.py
+++ src/corporacreator/preprocessors/fr.py
@@ -8,5 +8,19 @@ def fr(client_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
-    # TODO: Clean up fr data
+    # this if loop will skip the following else loop in the case that
+    # the sentence is purely Welsh characters as per https://github.com/
+    # unicode-cldr/cldr-misc-full/blob/master/main/ca/characters.json
+    if set(list(sentence)).issubset(set([' ', '-', 'a', 'à', 'â', 'æ', 'b', 'c',
+                                         'ç', 'd', 'e', 'é', 'è', 'ê', 'ë', 'f',
+                                         'g', 'h', 'i', 'î', 'ï', 'j', 'k', 'l',
+                                         'm', 'n', 'o', 'ô', 'œ', 'p', 'q', 'r',
+                                         's', 't', 'u', 'ù', 'û', 'ü', 'v', 'w',
+                                         'x', 'y', 'ÿ', 'z'])):
+        pass
+    else:
+        # TODO: Clean up fr data
+        pass
+    
+    
     return sentence
