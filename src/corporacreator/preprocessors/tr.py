9f149c654f8c58657a4bb3c3049bba1bfe6eaf76
josh <jmeyer@mozilla.com>
2019-01-29 21:15:02 +0300

    turkish

--- src/corporacreator/preprocessors/tr.py
+++ src/corporacreator/preprocessors/tr.py
@@ -8,5 +8,17 @@ def tr(client_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
-    # TODO: Clean up tr data
+    
+    # this if loop will skip the following else loop in the case that
+    # the sentence is purely Turkish characters as per https://github.com/
+    # unicode-cldr/cldr-misc-full/blob/master/main/tr/characters.json
+    if set(list(sentence)).issubset(set([' ', 'a', 'b', 'c', 'ç', 'd', 'e', 'f',
+                                         'g', 'ğ', 'h', 'ı', 'i', 'İ', 'j', 'k',
+                                         'l', 'm', 'n', 'o', 'ö', 'p', 'r', 's',
+                                         'ş', 't', 'u', 'ü', 'v', 'y', 'z'])):
+        pass
+    else:
+        # TODO: Clean up turkish data
+        pass
+    
     return sentence
