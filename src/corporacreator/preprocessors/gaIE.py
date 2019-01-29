4838ae5991e9e01e0e541ac1bf2ead5be6d72eae
josh <jmeyer@mozilla.com>
2019-01-29 20:25:02 +0300

    irish-gaelic

--- src/corporacreator/preprocessors/gaIE.py
+++ src/corporacreator/preprocessors/gaIE.py
@@ -8,5 +8,17 @@ def gaIE(client_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
-    # TODO: Clean up ga-IE data
+    # this if loop will skip the following else loop in the case that
+    # the sentence is purely Irish Gaelic characters as per https://github.com/
+    # unicode-cldr/cldr-misc-full/blob/master/main/ga/characters.json
+    if set(list(sentence)).issubset(set([' ', 'a', 'á', 'b', 'c', 'd', 'e', 'é',
+                                         'f', 'g', 'h', 'i', 'í', 'l', 'm', 'n',
+                                         'o', 'ó', 'p', 'r', 's', 't', 'u',
+                                         'ú'])):
+        pass
+    else:
+        # TODO: Clean up ga-IE data
+        pass
+    
+    
     return sentence
