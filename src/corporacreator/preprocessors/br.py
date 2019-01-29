5f7611a3f235bb4546b6871014a4e255d1bda4e2
josh <jmeyer@mozilla.com>
2019-01-29 19:57:27 +0300

    english

--- src/corporacreator/preprocessors/br.py
+++ src/corporacreator/preprocessors/br.py
@@ -8,18 +8,5 @@ def br(client_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
-    sentence = sentence.replace("'", "ʼ")
-
-        # this if loop will skip the following else loop in the case that
-    # the sentence is purely Welsh characters as per https://github.com/
-    # unicode-cldr/cldr-misc-full/blob/master/main/br/characters.json
-    if set(list(sentence)).issubset(set([' ', 'a', 'b', 'c', 'ʼ', 'd', 'e', 'ê',
-                                         'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                                         'n', 'ñ', 'o', 'p', 'r', 's', 't', 'u',
-                                         'ù', 'v', 'w', 'x', 'y', 'z'])):
-        pass
-    else:
-        # TODO: Clean up non-standard br data here
-        pass
-    
+    # TODO: Clean up br data
     return sentence
