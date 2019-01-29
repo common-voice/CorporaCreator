fc31fa056fabdc81d562fd3f14ea462121c516be
josh <jmeyer@mozilla.com>
2019-01-29 19:10:13 +0300

    catalan

--- src/corporacreator/preprocessors/ca.py
+++ src/corporacreator/preprocessors/ca.py
@@ -8,5 +8,16 @@ def ca(client_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
+    # this if loop will skip the following else loop in the case that
+    # the sentence is purely Welsh characters as per https://github.com/
+    # unicode-cldr/cldr-misc-full/blob/master/main/ca/characters.json
+    if set(list(sentence)).issubset(set([' ', '·', 'a', 'à', 'b', 'c', 'ç', 'd',
+                                         'e', 'é', 'è', 'f', 'g', 'h', 'i', 'í',
+                                         'ï', 'j', 'k', 'l', 'm', 'n', 'o', 'ó',
+                                         'ò', 'p', 'q', 'r', 's', 't', 'u', 'ú',
+                                         'ü', 'v', 'w', 'x', 'y', 'z'])):
+        pass
+    else:
+        pass
     # TODO: Clean up ca data
     return sentence
