b2994ff5d4201b56ed3a6f5a750725f694fcd4f5
josh <jmeyer@mozilla.com>
2019-01-29 19:56:32 +0300

    english

--- src/corporacreator/preprocessors/en.py
+++ src/corporacreator/preprocessors/en.py
@@ -8,5 +8,17 @@ def en(client_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
-    # TODO: Clean up en data
+    
+    # this if loop will skip the following else loop in the case that
+    # the sentence is purely English characters as per https://github.com/
+    # unicode-cldr/cldr-misc-full/blob/master/main/en/characters.json
+    if set(list(sentence)).issubset(set([' ', '-', 'a', 'b', 'c', 'd', 'e', 'f',
+                                         'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                         'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                         'w', 'x', 'y', 'z'])):
+        pass
+    else:
+        # TODO: Clean up en data
+        pass
+    
     return sentence
