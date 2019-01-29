b2718cf6bd58947a2446ab5ad18f47111ee8d3da
josh <jmeyer@mozilla.com>
2019-01-29 21:02:08 +0300

    dutch

--- src/corporacreator/preprocessors/nl.py
+++ src/corporacreator/preprocessors/nl.py
@@ -8,5 +8,17 @@ def nl(user_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
-    # TODO: Clean up nl data
+    # this if loop will skip the following else loop in the case that
+    # the sentence is purely Dutch characters as per https://github.com/
+    # unicode-cldr/cldr-misc-full/blob/master/main/nl/characters.json
+    if set(list(sentence)).issubset(set([' ', 'a', 'á', 'ä', 'b', 'c', 'd', 'e',
+                                         'é', 'ë', 'f', 'g', 'h', 'i', 'í', 'ï',
+                                         'j', 'k', 'l', 'm', 'n', 'o', 'ó', 'ö',
+                                         'p', 'q', 'r', 's', 't', 'u', 'ú', 'ü',
+                                         'v', 'w', 'x', 'y', 'z'])):
+        pass
+    else:
+        # TODO: Clean up dutch data
+        pass
+    
     return sentence
