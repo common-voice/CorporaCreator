4facb7ebbfc601bc4354dbd3a30af35440176edc
josh <jmeyer@mozilla.com>
2019-01-29 20:09:04 +0300

    esperanto

--- src/corporacreator/preprocessors/eo.py
+++ src/corporacreator/preprocessors/eo.py
@@ -8,5 +8,17 @@ def eo(user_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
-    # TODO: Clean up eo data
+    
+    # this if loop will skip the following else loop in the case that
+    # the sentence is purely Esperanto characters as per https://github.com/
+    # unicode-cldr/cldr-misc-full/blob/master/main/eo/characters.json
+    if set(list(sentence)).issubset(set([' ', 'a', 'b', 'c', 'ĉ', 'd', 'e', 'f',
+                                         'g', 'ĝ', 'h', 'ĥ', 'i', 'j', 'ĵ', 'k',
+                                         'l', 'm', 'n', 'o', 'p', 'r', 's', 'ŝ',
+                                         't', 'u', 'ŭ', 'v', 'z'])):
+        pass
+    else:
+        # TODO: Clean up eo data
+        pass
+   
     return sentence
