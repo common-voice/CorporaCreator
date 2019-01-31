c02b22ad51548e180c3a857b536f1c2b123e7446
josh <jmeyer@mozilla.com>
2019-01-31 23:48:36 +0100

    kyrgyz mis-typed "oe"

--- src/corporacreator/preprocessors/ky.py
+++ src/corporacreator/preprocessors/ky.py
@@ -9,5 +9,6 @@ def ky(client_id, sentence):
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
     sentence = sentence.replace("•", "")
+    sentence = sentence.replace("ѳ", "ө")
     # TODO: ⅛, ИНН, КСДП, other abbreviations
     return sentence
