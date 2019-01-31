be3eff613916f69d3411ccd316a8d5f175ba32be
josh <jmeyer@mozilla.com>
2019-01-31 16:59:33 +0100

    took out alphabet checking

--- src/corporacreator/preprocessors/cy.py
+++ src/corporacreator/preprocessors/cy.py
@@ -1,12 +1,3 @@
-# compile the set of the alphabet just once
-alphabet = set([' ', 'a','á', 'à', 'â', 'ä', 'b', 'c',
-                'd', 'e', 'é', 'è', 'ê', 'ë', 'f', 'g',
-                'h', 'i', 'í', 'ì', 'î', 'ï', 'j', 'l',
-                'm', 'n', 'o', 'ó', 'ò', 'ô', 'ö', 'p',
-                'r', 's', 't', 'u', 'ú', 'ù', 'û', 'ü',
-                'w', 'ẃ', 'ẁ', 'ŵ', 'ẅ', 'y', 'ý', 'ỳ',
-                'ŷ', 'ÿ'])
-
 def cy(client_id, sentence):
     """
     Cleans up the passed sentence, removing or reformatting invalid data.
@@ -20,19 +11,9 @@ def cy(client_id, sentence):
     # TODO: geiriau Saesneg / English inspired/pronunced words:
     # wallace, celsius, ddiesel, wicipedia, william, chiswell, f., h.
 
-    # this if loop will skip the following else loop in the case that
-    # the sentence is purely Welsh characters as per https://github.com/
-    # unicode-cldr/cldr-misc-full/blob/master/main/cy/characters.json
-    # alphabet is compiled at the beginning of this script
-    if set(sentence.lower()).issubset(alphabet):
-        pass
-    else:
-        # Not all chars are in the Welsh alphabet!
-        # more processing needed here!
-        sentence = sentence.replace(",", "") # remove commas
-        sentence = sentence.replace("’", "'") # fix apostrophes
-        sentence = sentence.replace("wwna", "wna")
-        sentence = sentence.replace(" siwr ", " siŵr ")
-        sentence = sentence.replace("\\tungellog"," ungellog")
+    sentence = sentence.replace("’", "'") # fix apostrophes
+    sentence = sentence.replace("wwna", "wna")
+    sentence = sentence.replace(" siwr ", " siŵr ")
+    sentence = sentence.replace("\\tungellog"," ungellog")
         
     return sentence
