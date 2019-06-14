e66277f59ac276579e46766fcf8a201e2c4dd820
aymen <aymen.bensalem@enis.tn>
2019-06-14 16:42:16 +0100

    Éliminer les abréviations et les symboles inattendus

--- src/corporacreator/preprocessors/fr.py
+++ src/corporacreator/preprocessors/fr.py
@@ -1,3 +1,18 @@
+import re
+
+# All special characters
+FILTER_SYMBOLES_REG = re.compile(
+    r'[\{\}\[\]«»_\|\(\)\\…"(^—)=&\ô*'
+    r'/µ#’@℗`~¹½¼¾¿º±↨↑↓▼→▲←↔∟§°‼¸‰'
+    r'‘¶“”•—´☺☻♥♦♠♣•◘○◙♂►♀☼♫♪¢¦Ξ≈˜†'
+    r'√ƒοΔδΛΓκιςζυσρΣγτθΘφΦηχξβωγΩΨ◊░▒▓'
+    r'│├╚┼┬┴└┐┤╝╗╬╣║ßÞ═™›³ª¯¬®]+')
+
+# Detect abreviation ex: TVA, T V A
+EXCLUDE_ABBREVIATION_REG = re.compile(r'([A-Z]){2,3}|(( [A-Z] )( ?[A-Z]){1, })')
+
+
+
 def fr(client_id, sentence):
     """Cleans up the passed sentence, removing or reformatting invalid data.
 
@@ -9,4 +24,6 @@ def fr(client_id, sentence):
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
     # TODO: Clean up fr data
+
+    sentence = FILTER_SYMBOLES_REG.sub('', sentence)
     return sentence
