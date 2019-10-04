de131e4da07b0fd1af417671e31188ca2c9cdc24
Artie <54867745+artie-inc@users.noreply.github.com>
2019-10-04 16:41:59 -0700

    C++ --> C plus plus
    there are 160 utterances with C++ spoken in common voice english, and this user is in the `test.csv` file after `test.tsv` is passed through `import_cv2.py`, and I verified that it is spoken this way

--- src/corporacreator/preprocessors/en.py
+++ src/corporacreator/preprocessors/en.py
@@ -8,6 +8,8 @@ def en(client_id, sentence):
     Returns:
       (str): Cleaned up sentence. Returning None or a `str` of whitespace flags the sentence as invalid.
     """
+    if client_id == "3c8f07827758e9ce8070ed287139d6d3e6457c1c16dcb972edac76c4d4333dc9e9c428711237e2b34fa29f4d249287fd238ac884534e33075958233643bbd0a1":
+        sentence = sentence.replace("C++", "C plus plus")
     ## collapse all apostrophe-like marks
     ## e.g. common_voice_en_18441344.mp3	‘I’m not a serpent!’ --> 'I'm not a serpent!'
     sentence = sentence.replace("’","'") # right-ticks --> apostrophes
